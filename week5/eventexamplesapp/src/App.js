d7deb4d86a2db6ce4e6901b76729e277b6f6a492
Akhila PS <akhilaps1604@gmail.com>
2026-07-18 23:15:49 +0530

    React excercises

---
+++ week5/eventexamplesapp/src/App.js
@@ -0,0 +1,24 @@
+import EventExamples from "./components/EventExamples";
+import CurrencyConvertor from "./components/CurrencyConvertor";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <h1>React Event Examples</h1>
+
+      <EventExamples />
+
+      <hr />
+
+      <CurrencyConvertor />
+
+    </div>
+
+  );
+
+}
+
+export default App;
